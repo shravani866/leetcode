96e56d925e1b52583fe5b0104b04bbe2d9870014
shravani866 <shravaniumanabadi9@gmail.com>
2026-03-24 14:50:10 +0530

    Time: 47 ms (64.00%) | Memory: 12.3 MB (100.00%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,5 +1,14 @@
 class Solution:
     def minOperations(self, nums, k):
-        if min(nums) < k:
+        
+        if any(num < k for num in nums):
             return -1
-        return len(set(num for num in nums if num > k))
+        
+       
+        unique_values = set()
+        
+        for num in nums:
+            if num > k:
+                unique_values.add(num)
+        
+        return len(unique_values)
