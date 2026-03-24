74181ee39d417972f629268b33ce829db45e8daf
shravani866 <shravaniumanabadi9@gmail.com>
2026-03-24 15:08:29 +0530

    Time: 7 ms (40.74%) | Memory: 12.4 MB (40.74%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumOperations(self, nums):
+        e = 0
+        
+        while len(nums) > len(set(nums)):
+            nums = nums[3:]
+            e += 1
+            
+        return e
