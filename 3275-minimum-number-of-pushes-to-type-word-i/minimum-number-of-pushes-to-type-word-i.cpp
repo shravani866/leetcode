f6d8db38dedabb9003df9a90c0e844fa9dca9764
shravani866 <shravaniumanabadi9@gmail.com>
2026-07-30 11:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,7 +3,18 @@ public:
     int minimumPushes(string word) {
         int ans = 0;
         for(int i = 0; i < word.size(); i++){
-            ans += (i/8)+1;
+            if(i < 8){
+                ans += 1;
+            }
+            else if(i < 16){
+                ans += 2;
+
+            }else if(i < 24){
+                ans += 3;
+            }
+            else{
+                ans += 4;
+            }
         }
         return ans;
     }
